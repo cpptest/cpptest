3902e9c37063b365cef2000399ef707d9649404d
Niklas Lundell <nilu@users.sourceforge.net>
2003-05-13 13:34:13 +0000

    Initial import

---
+++ cpptest/src/missing.h
@@ -0,0 +1,52 @@
+// ---
+//
+// $Id: missing.h,v 1.1 2003/05/13 13:34:13 nilu Exp $
+//
+// C++Test - A C++ Unit Testing Framework
+// Copyright (c) 2003 Niklas Lundell
+//
+// ---
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the
+// Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+// Boston, MA 02111-1307, USA.
+//
+// ---
+
+#ifndef CPPTEST_MISSING_H
+#define CPPTEST_MISSING_H
+
+#include "config.h"
+
+namespace Test
+{
+#ifndef HAVE_GETTIMEOFDAY
+
+	struct timeval
+	{
+		long tv_sec;
+		long tv_usec;
+	};
+		
+	extern int gettimeofday(timeval* tv, void*);
+		
+#endif // #ifndef HAVE_GETTIMEOFDAY
+		
+#ifndef HAVE_ROUND
+	extern double round(double d);
+#endif	
+	
+} // namespace Test
+
+#endif // #ifndef CPPTEST_MISSING_H
